f8844a2562dfe1e85ee33f1c46056fc7dfd0ad85
MrOrz <johnsonliang7@gmail.com>
2011-12-27 21:31:41 +0800

    jshint watchr.js

--- watchr.js
+++ watchr.js
@@ -1,16 +1,17 @@
+/*global chrome */
 /*
  * watchr.js
  *
  * injected content script
  *
 */
-;(function(window, document, undefined){
+(function(window, document, undefined){
   "use strict";
   var initTime = Date.now();
-  
+
   document.onready = function() {
     chrome.extension.sendRequest({"time": initTime}, function(response) {
       console.log("Request send");
     });
   };
-})(window, document);
+}(window, document));
