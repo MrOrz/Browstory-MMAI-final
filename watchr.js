7f36c384e48693d4d634444ac923be533b7ee399
MrOrz <johnsonliang7@gmail.com>
2012-01-05 17:14:32 +0800

    content script container getter complete. It works on many sites except google search result pages...orz

--- watchr.js
+++ watchr.js
@@ -7,7 +7,65 @@
 */
 (function(window, document, undefined){
   "use strict";
-  var initTime = Date.now();
+  var
+    blacklist = 'script,meta,embed,link,object', // HTML elements to ignore
+    bodyWidth = $('body').innerWidth(),
+
+    // Recursively find container element.
+    // An element is called 'container' if its direct parent
+    // node has width >= bodyWidth and itself containing elements.
+    //
+    findContainer = function($root){
+      var
+        // select non-empty, non-blacklist children
+        $children = $root.children().not(blacklist).filter(function(){
+          return $(this).children().not(blacklist).length > 0;
+        }),
+        possibleContainers = [], ret = {};
+
+      // traversing current element's children if they are the parent of
+      // a container
+      //
+      $children.each(function(){
+        var $child = $(this), container;
+        // the container should reside on elements whose width >= bodyWidth.
+        // so use this child as root to find Container.
+        if($child.width() >= bodyWidth){
+          container = findContainer($child);
+          if(!$.isEmptyObject(container)){
+            possibleContainers.push(container);
+          }
+        }
+      })
+
+      // If no possible containers (maybe all children's width < bodyWidth)
+      // then we make all children be a possible container
+      //
+      if(possibleContainers.length === 0){
+        $children.each(function(){
+          possibleContainers.push($(this));
+        });
+      }
+
+      // Pick the best (=widest) container here.
+      //
+
+      ret._width = 0;
+      $.each(possibleContainers, function(){
+        // possibleContainers is already an array of jQuery obj
+        var $elem = this;
+        if($elem.width() > ret._width){
+          ret = $elem;
+          ret._width = $elem.width();
+        }
+      });
+      delete ret._width;
+      return ret;
+    },
+    initTime = Date.now();
+
+  // traversing DOM structure to get the root of contents
+  console.log('container found:', findContainer($('body')));
 
   // content script runs at document_idle
   chrome.extension.sendRequest({"time": initTime}, function(response) {
