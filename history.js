06577e3cbec19dd8697ad8c221e45ea1f7a720f9
MrOrz <johnsonliang7@gmail.com>
2012-01-06 22:51:19 +0800

    history last-viewed function implemented

--- history.js
+++ history.js
@@ -3,24 +3,28 @@
 $(function(){
   "use strict";
   var
-    $target = $('#body'),
-    $tmpl = $('#itemTemplate');
+    $tmpl = $('#itemTemplate'),
+    $recent_target = $('#recent-target'),
+    $empty = $tmpl.tmpl({}),
+    itemCount; // counting how many items in a row
+
+  // testing item width
+  //
+  $empty.appendTo($('#hidden'));
+  itemCount = Math.floor($recent_target.width() / $empty.outerWidth(true));
 
   $.initDB.done(function(db){
     db.readTransaction(function(tx){
       // clean up target
-      $target.empty();
+      $recent_target.empty();
 
-      tx.executeSql('SELECT * FROM entry;', [], function(tx, results){
-        var i, htmlStr = "", items = [], item;
+      tx.executeSql('SELECT * FROM entry ORDER BY lastview DESC LIMIT ?;',
+      [itemCount], function(tx, results){
+        var i, htmlStr = "", items = [];
         for (i = 0; i < results.rows.length; i+=1) {
-          item = results.rows.item(i);
-          items.push({
-            screenshot: item.screenshot,
-            url:        item.url
-          });
+          items.push(results.rows.item(i));
         }
-        $tmpl.tmpl(items).appendTo($target);
+        $tmpl.tmpl(items).appendTo($recent_target);
       }, function(){
         console.error('Read Transaction Error', arguments);
       });
