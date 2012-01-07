1a62518adde8d6563070f831b4ecff00c23b2aed
MrOrz <johnsonliang7@gmail.com>
2012-01-07 23:33:36 +0800

    styles & resize handler for history

--- javascripts/history.js
+++ javascripts/history.js
@@ -5,24 +5,61 @@ $(function(){
   "use strict";
   var
     $tmpl = $('#itemTemplate'),
-    $recent_target = $('#recent-target'),
-    $search_target = $('#search-target'),
+    $recentTarget = $('#recent-target'),
+    $searchTarget = $('#search-target'),
     $empty = $tmpl.tmpl({}),
-    itemCount; // counting how many items in a row
+    recentItems, searchResults, // caches items for redraw
+    itemCount, // counting how many items in a row
+    largeCount, smallCount, tinyCount,
+    // testing item width and populating *Count.
+    //
+    measure = function(){
+      itemCount = Math.floor($recentTarget.width() / $empty.outerWidth(true));
+      $empty.addClass('large');
+      largeCount = Math.floor($recentTarget.width() / $empty.outerWidth(true)); $empty.removeClass('large');
+      $empty.addClass('small');
+      smallCount = Math.floor($recentTarget.width() / $empty.outerWidth(true)); $empty.removeClass('small');
+      $empty.addClass('tiny');
+      tinyCount = Math.floor($recentTarget.width() / $empty.outerWidth(true)); $empty.removeClass('tiny');
+    },
+    // determines the size of item by returning the corresponding class name
+    itemClass = function(idx){
+      if(idx < largeCount){
+        return 'large';
+      }else if(idx < largeCount + itemCount){
+        return 'normal'; // unstyled; normal style.
+      }else if(idx < largeCount + itemCount + smallCount * 2){
+        return 'small';
+      }else{
+        return 'tiny';
+      }
+    },
+    insertBr = function($target){
+      var lastClassName = 'item large';
+      $target.remove('br').find('.item').each(function(){
+        if(this.className !== lastClassName){
+          lastClassName = this.className;
+          $('<br />').insertBefore(this);
+        }
+      });
+    };
 
-  // testing item width
-  //
   $empty.appendTo($('#hidden'));
-  itemCount = Math.floor($recent_target.width() / $empty.outerWidth(true));
+  measure();
 
+  // show recent
   $.initDB.done(function(db){
     db.readTransaction(function(tx){
       // clean up target
-      $recent_target.empty();
+      $recentTarget.empty();
 
-      tx.executeSql('SELECT * FROM entry ORDER BY lastview DESC LIMIT ?;',
-      [itemCount], function(tx, results){
-        $tmpl.tmpl($.getItems(results)).appendTo($recent_target);
+      tx.executeSql('SELECT * FROM entry ORDER BY lastview DESC;',
+      [], function(tx, results){
+        recentItems = $.getItems(results); // populate cache with search result
+        $tmpl.tmpl(recentItems).each(function(idx){
+          $(this).addClass(itemClass(idx));
+        }).appendTo($recentTarget);
+        insertBr($recentTarget);
       }, function(){
         console.error('Read Transaction Error', arguments);
       });
@@ -43,9 +80,9 @@ $(function(){
         $.initDB.done(function(db){
           db.readTransaction(function(tx, results){
             console.log('SELECT * FROM entry WHERE url IN ('+ urls + ') ORDER BY lastview DESC;');
-            tx.executeSql('SELECT * FROM entry WHERE url IN ('+ urls + ') ORDER BY lastview DESC;', 
+            tx.executeSql('SELECT * FROM entry WHERE url IN ('+ urls + ') ORDER BY lastview DESC;',
             [], function(tx, results){
-              $search_target.empty().append($tmpl.tmpl($.getItems(results)));
+              $searchTarget.empty().append($tmpl.tmpl($.getItems(results)));
             });
           },function(){
             console.error('read transaction error on search', arguments);
@@ -53,5 +90,18 @@ $(function(){
         });
       });
     }, 200);
+  });
+  $(window).resize(function(){
+    // recalculate *counts and render the elements again
+    measure();
+    var last_cls = 'large';
+    $recentTarget.children('br').remove().end()
+     .find('.item').each(function(idx){
+      var $item = $(this), cls = itemClass(idx);
+      if(!$item.hasClass(cls)){
+        $item.removeClass('large normal small tiny').addClass(cls);
+      }
+    });
+    insertBr($recentTarget);
   })
 });
