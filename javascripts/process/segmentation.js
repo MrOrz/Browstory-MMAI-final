59ad69e5f6266fb3ccea82a31c6629cc5c90457c
MrOrz <johnsonliang7@gmail.com>
2012-01-08 10:40:44 +0800

    jshint segmentation & util.js

--- javascripts/process/segmentation.js
+++ javascripts/process/segmentation.js
@@ -1,11 +1,14 @@
-"use strict"
+/*global Pixastic, util */
+
 function segmentation(img, rect, debug){
+	"use strict";
 
 	// coloring context
 	var ctx = img.getContext('2d'),
-	 		colors = ['#ff0000', '#00ff00'],
-	 		i;
-	for(i=0; i<rect.length;  ++i){
+			colors = ['#ff0000', '#00ff00'],
+			i,
+			cropimage;
+	for(i=0; i<rect.length; ++i){
 		ctx.fillStyle = colors[i];
 		ctx.fillRect(rect[i].left, rect[i].top,
 			rect[i].width, rect[i].height);
@@ -14,32 +17,35 @@ function segmentation(img, rect, debug){
 	// start segmentation
 	var hor = new Array(3),
 		ver = new Array(3),
-		i,j,k,cut,maxcut,maxdiff,diff,
+		j,k,cut,maxcut,maxdiff,diff,
 		width = img.width, height = img.height;
 	hor[0] = ver[0] = hor[3] = ver[3] = 0;
 	for(i=1; i<=7; i+=2)
 	{
-		var	cropimage = Pixastic.process(img, "crop", {
+		cropimage = Pixastic.process(img, "crop", {
 			rect : {
-				left : Math.floor(width/3)*(i%3), top : Math.floor(height/3)*Math.floor(i/3), width : Math.ceil(width/3), height : Math.ceil(height/3)
+				left   : Math.floor(width/3)*(i%3),
+				top    : Math.floor(height/3)*Math.floor(i/3),
+				width  : Math.ceil(width/3),
+				height : Math.ceil(height/3)
 			}
-			});
-		if(i%3==1)
+		});
+		if(i%3===1)
 		{
 			maxdiff = -1;
 			for(cut=1; cut<Math.ceil(height/3); cut+=5)
 			{
 				var top = cropimage.getContext('2d').getImageData(0, 0, Math.ceil(width/3), cut).data,
 					bottom = cropimage.getContext('2d').getImageData(0, cut, Math.ceil(width/3), Math.ceil(height/3)-cut).data;
-				diff = distance(top,bottom);
-				if(maxdiff==-1||diff>maxdiff)
+				diff = util.distance(top,bottom);
+				if(maxdiff===-1||diff>maxdiff)
 				{
 					maxdiff = diff;
 					maxcut = cut;
 				}
 			}
 			//maxcut=50;
-			hor[(i-1)/6+1] = ((i-1)/6+1==2)?maxcut:Math.ceil(height/3)-maxcut;
+			hor[(i-1)/6+1] = ((i-1)/6+1===2)?maxcut:Math.ceil(height/3)-maxcut;
 		}
 		else
 		{
@@ -48,45 +54,53 @@ function segmentation(img, rect, debug){
 			{
 				var left = cropimage.getContext('2d').getImageData(0, 0, cut, Math.ceil(height/3)).data,
 					right = cropimage.getContext('2d').getImageData(cut, 0, Math.ceil(width/3)-cut, Math.ceil(height/3)).data;
-				diff = distance(left,right);
-				if(maxdiff==-1||diff>maxdiff)
+				diff = util.distance(left,right);
+				if(maxdiff===-1||diff>maxdiff)
 				{
 					maxdiff = diff;
 					maxcut = cut;
 				}
 			}
 			//maxcut=50;
-			ver[(i-1)/2] = ((i-1)/2==2)?maxcut:Math.ceil(width/3)-maxcut;
+			ver[(i-1)/2] = ((i-1)/2===2)?maxcut:Math.ceil(width/3)-maxcut;
 		}
 	}
 
 	// Similarity test among nine blocks
-	var adj = new Array(8);
-		adj[0] = [1,3];
-		adj[1] = [4];
-		adj[2] = [1,5];
-		adj[3] = [4];
-		adj[4] = [7];
-		adj[5] = [4];
-		adj[6] = [3,7];
-		adj[7] = [8];
-		adj[8] = [5];
-	var cropimage = new Array(8),
-		L = new Array(8),
-		C = new Array(8),
-		thr = 0.002;
+	var adj = new Array(8),
+			L = new Array(8),
+			C = new Array(8),
+			thr = 0.002;
+
+	adj[0] = [1,3];
+	adj[1] = [4];
+	adj[2] = [1,5];
+	adj[3] = [4];
+	adj[4] = [7];
+	adj[5] = [4];
+	adj[6] = [3,7];
+	adj[7] = [8];
+	adj[8] = [5];
+	cropimage = new Array(8);
+
 	for(i=0; i<=8; i++)
 	{
 		L[i]=i;
-		cropimage[i] = img.getContext('2d').getImageData(Math.floor(width/3)*(i%3)+(i%3==1?-1:1)*ver[i%3], Math.floor(height/3)*Math.floor(i/3)+(Math.floor(i/3)==1?-1:1)*hor[Math.floor(i/3)], Math.ceil(width/3)-(i%3==1?-1:1)*ver[i%3]-(i%3==1?-1:1)*ver[i%3+1], Math.ceil(height/3)-(Math.floor(i/3)==1?-1:1)*hor[Math.floor(i/3)]-(Math.floor(i/3)==1?-1:1)*hor[Math.floor(i/3)+1]);
-		C[i] = ColorF(cropimage[i].data);
+		cropimage[i] = img.getContext('2d').getImageData(
+			Math.floor(width/3) * (i%3) + (i%3===1?-1:1) * ver[i%3],
+			Math.floor(height/3) * Math.floor(i/3) + (Math.floor(i/3)===1?-1:1)*hor[Math.floor(i/3)],
+			Math.ceil(width/3)-(i%3===1?-1:1)*ver[i%3]-(i%3===1?-1:1)*ver[i%3+1],
+			Math.ceil(height/3)-(Math.floor(i/3)===1?-1:1)*hor[Math.floor(i/3)] - 
+				(Math.floor(i/3)===1?-1:1)*hor[Math.floor(i/3)+1]
+		);
+		C[i] = util.ColorF(cropimage[i].data);
 	}
 	var canvas = Pixastic.process(img, "crop", {
 			rect : {
 				left : 0, top : 0, width : width, height : height
 			}
 			});
-	var ctx = canvas.getContext('2d');
+	ctx = canvas.getContext('2d');
 	ctx.fillStyle="#000";
 	ctx.fillRect(0,Math.floor(height/3)-hor[1],width,2);
 	ctx.fillRect(0,Math.floor(height/3)*2+hor[2],width,2);
@@ -94,25 +108,28 @@ function segmentation(img, rect, debug){
 	ctx.fillRect(Math.floor(width/3)*2+ver[2],0,2,height);
 	for(i=0; i<=8; i++) //cluster among adjacent blocks
 	{
-		if(cropimage[i].height<height/24)
+		if(cropimage[i].height<height/24){
 			L[i]=L[3+i%3]=Math.min(L[i],L[3+i%3]);
-		else if(cropimage[i].width<width/24)
+		}
+		else if(cropimage[i].width<width/24){
 			L[i]=L[Math.floor(i/3)*3+1]=Math.min(L[i],L[Math.floor(i/3)*3+1]);
+		}
 		else
 		{
 			maxdiff = -1;
 			for(j=0; j<adj[i].length; j++)
 			{
-				diff = distance(cropimage[i].data,cropimage[adj[i][j]].data);
+				diff = util.distance(cropimage[i].data,cropimage[adj[i][j]].data);
 				//console.log('diff('+i+','+adj[i][j]+')='+diff);
-				if(diff<maxdiff||maxdiff==-1)
+				if(diff<maxdiff||maxdiff===-1)
 				{
 					maxdiff = diff;
 					k = adj[i][j];
 				}
 			}
-			if(maxdiff<thr)
+			if(maxdiff<thr){
 				L[i]=L[k]=Math.min(L[i],L[k]);
+			}
 		}
 	}
 
@@ -131,13 +148,13 @@ function segmentation(img, rect, debug){
 	// Horrizontal
 	for(i=0; i<=2; i++)
 	{
-		if(L[3*i]==L[3*i+1]&&L[3*i]==L[3*i+2])
+		if(L[3*i]===L[3*i+1]&&L[3*i]===L[3*i+2])
 		{
 			F[i] = 1;
 		}
 		else
 		{
-			if(L[3*i]==L[3*i+1]||L[3*i]==L[3*i+2]||L[3*i+1]==L[3*i+2])
+			if(L[3*i]===L[3*i+1]||L[3*i]===L[3*i+2]||L[3*i+1]===L[3*i+2])
 			{
 				F[i] = 2;
 			}
@@ -150,13 +167,13 @@ function segmentation(img, rect, debug){
 	// Vertical
 	for(i=0; i<=2; i++)
 	{
-		if(L[i]==L[i+3]&&L[i]==L[i+6])
+		if(L[i]===L[i+3]&&L[i]===L[i+6])
 		{
 			F[i+3] = 1;
 		}
 		else
 		{
-			if(L[i]==L[i+3]||L[i]==L[i+6]||L[i+3]==L[i+6])
+			if(L[i]===L[i+3]||L[i]===L[i+6]||L[i+3]===L[i+6])
 			{
 				F[i+3] = 2;
 			}
@@ -166,7 +183,15 @@ function segmentation(img, rect, debug){
 			}
 		}
 	}
-	console.log('F:'+F);
-	F.canvas = canvas;
-	return F;
+	var C1d = [];
+	for(i=0; i<C.length; ++i){
+		C1d = C1d.concat(C[i]);
+	}
+
+	console.log('F:', F, 'C:', C1d);
+	return {
+		structure: F,
+		canvas: canvas,
+		colormap: C1d
+	};
 }
--- javascripts/process/util.js
+++ javascripts/process/util.js
@@ -1,131 +1,139 @@
-"use strict"
-function distance(img1,img2){
-	var hist1=histHSV(img1),
-		hist2=histHSV(img2),
-		sum = 0, i;
-	for(i=0; i<hist1.length; i++)
-	{
-		sum += (hist1[i]-hist2[i])*(hist1[i]-hist2[i]);
-	}
-	return sum;
-}
+var util = (function (){
+	"use strict";
+	function RGB2HSV(R,G,B){// RGB to HSV
+		var r=R/255, g=G/255, b=B/255, H, S, V;
+		var minRGB = Math.min(r,g,b),
+				maxRGB = Math.max(r,g,b);
 
-function ColorF(img){
-	var hist = histHSV(img), i, max=-1, second=-1,
-		maxindex = new array(1), C = new array(3);
-	// Find dominant colors
-	for(i=0; i<hist.length; i++)
-	{
-		if(hist[i]>max)
-		{
-			second=max;
-			maxindex[1]=maxindex[0];
-			max=hist[i];
-			maxindex[0]=i;
+		// Black-gray-white
+		if (minRGB===maxRGB) {
+				V = minRGB;
+				return [0,0,V*0.9999];
 		}
-		else if(hist[i]>second)
-		{
-			second=hist[i];
-			maxindex[1]=i;
-		}
+
+		// Colors other than black-gray-white:
+		var d = (r===minRGB) ? g-b : ((b===minRGB) ? r-g : b-r);
+		var h = (r===minRGB) ? 3 : ((b===minRGB) ? 1 : 5);
+		H = 60*(h - d/(maxRGB - minRGB));
+		S = (maxRGB - minRGB)/maxRGB;
+		V = maxRGB;
+		return [H*0.9999,S*0.9999,V*0.9999];
 	}
-	
-	// Quantization
-	for(i=0; i<2; i++)
-	{
-		if(maxindex[i]<162)
-		{
-			var H = Math.floor(maxindex[i]/9);
-			C[2*i] = Math.floor(H/45)*45+22.5;
-			C[2*i+1] = H<22.5?360:Math.ceil((H-22.5)/45)*45;
+	function histHSV(imageData){
+		//var imageData = img.getContext('2d').getImageData(0, 0, img.width, img.height).data;
+		var hist = [], i, H, S, V, HSV, imageSize = imageData.length/4;
+
+		// H: 18 bins, S:3 bins, V:3 bins, gray: 4 bins
+		// Pixel with HSV = [h,s,v] are put into bin number 3*3*h + 3*s + v,
+		// where h in [0, 18), s in [0, 3) and v in [0, 3),
+		// and gray pixels are put in bin number 162~165.
+		for(i=0; i<166; i+=1){
+			hist[i] = 0;
 		}
-		else
-		{
-			var S = maxindex[i]-162;
-			C[2*i] = C[2*i+1] = -S;
+
+		for(i=0; i<imageSize*4; i+=4){ // histogram count
+			//R:imageData[i] G:imageData[i+1] B:imageData[i+2]
+			HSV=RGB2HSV(imageData[i], imageData[i+1], imageData[i+2]);
+			H = Math.floor(HSV[0]/360 * 18);
+			S = Math.floor(HSV[1] * 4); // 3+1 extra bin for gray pixels
+			V = Math.floor(HSV[2] * 3);
+
+		// gray pixels, saturation is low
+			if(S === 0){
+				 // quantize the HSV[2] into 4 steps
+			 hist[162 + Math.floor(HSV[2]*4)] += 1;
+			 continue;
+			}
+
+			// shift S from 1~3 to 0~2
+			S -= 1;
+			if(9*H+3*S+V >= 166){ console.log('HSV = ',H,S,V); }
+			hist[9*H+3*S+V] += 1;
 		}
+		for(i=0; i<166; i+=1){
+			hist[i]/=imageSize;
+		}
+		return hist;
 	}
-	return C;
-}
-
-function RGB2HSV(R,G,B){// RGB to HSV
-    var r=R/255, g=G/255, b=B/255, H, S, V;
-    var minRGB = Math.min(r,g,b),
-        maxRGB = Math.max(r,g,b);
-
-    // Black-gray-white
-    if (minRGB==maxRGB) {
-        V = minRGB;
-        return [0,0,V*0.9999];
-    }
-
-    // Colors other than black-gray-white:
-    var d = (r==minRGB) ? g-b : ((b==minRGB) ? r-g : b-r);
-    var h = (r==minRGB) ? 3 : ((b==minRGB) ? 1 : 5);
-    H = 60*(h - d/(maxRGB - minRGB));
-    S = (maxRGB - minRGB)/maxRGB;
-    V = maxRGB;
-    return [H*0.9999,S*0.9999,V*0.9999];
-}
-
-function histHSV(imageData){
-   //var imageData = img.getContext('2d').getImageData(0, 0, img.width, img.height).data;
-   var hist = [], i, H, S, V, HSV, imageSize = imageData.length/4;
-
-   // H: 18 bins, S:3 bins, V:3 bins, gray: 4 bins
-   // Pixel with HSV = [h,s,v] are put into bin number 3*3*h + 3*s + v,
-   // where h in [0, 18), s in [0, 3) and v in [0, 3),
-   // and gray pixels are put in bin number 162~165.
-   for(i=0; i<166; i+=1){
-      hist[i] = 0;
-   }
-
-   for(i=0; i<imageSize*4; i+=4){ // histogram count
-        //R:imageData[i] G:imageData[i+1] B:imageData[i+2]
-        HSV=RGB2HSV(imageData[i], imageData[i+1], imageData[i+2]);
-        H = Math.floor(HSV[0]/360 * 18);
-        S = Math.floor(HSV[1] * 4); // 3+1 extra bin for gray pixels
-        V = Math.floor(HSV[2] * 3);
-
-      // gray pixels, saturation is low
-        if(S === 0){
-           // quantize the HSV[2] into 4 steps
-         hist[162 + Math.floor(HSV[2]*4)] += 1;
-         continue;
-        }
-
-        // shift S from 1~3 to 0~2
-        S -= 1;
-        if(9*H+3*S+V >= 166) console.log('HSV = ',H,S,V)
-        hist[9*H+3*S+V] += 1;
-   }
-	for(i=0; i<166; i+=1){
-		hist[i]/=imageSize;
+	function distance(img1,img2){
+		var hist1=histHSV(img1),
+			hist2=histHSV(img2),
+			sum = 0, i;
+		for(i=0; i<hist1.length; i++)
+		{
+			sum += (hist1[i]-hist2[i])*(hist1[i]-hist2[i]);
+		}
+		return sum;
 	}
-   return hist;
-}
+	function ColorF(img){
+		var hist = histHSV(img), i, max=-1, second=-1,
+			maxindex = new Array(1), C = new Array(3);
+		// Find dominant colors
+		for(i=0; i<hist.length; i++)
+		{
+			if(hist[i]>max)
+			{
+				second=max;
+				maxindex[1]=maxindex[0];
+				max=hist[i];
+				maxindex[0]=i;
+			}
+			else if(hist[i]>second)
+			{
+				second=hist[i];
+				maxindex[1]=i;
+			}
+		}
 
-function showHist(hist){
-    var h, s, v, $target = $('<div>'), option;
-    for(h=0; h<18; ++h){
-        for(s=0; s<3; ++s){
-            for(v=0; v<3; ++v){
-                option = {style: 'background: hsl('+(h/18*360)+','+((s+1)/4*100)+'%,'+(v/3*100)+'%);'};
-                if(v === 0){ // make text white if bg is dark
-                    option.style += 'color: #fff;';
-                }
-                $target.append($('<span>', option).text(hist[9*h+3*s+v]));
-            }
-        }
-        $target.append($('<br />'));
-    }
-    for(v=0; v<4; ++v){
-       option = {style: 'background: hsl(0,0%,'+(v/4*100)+'%);'};
-       if(v === 0){ // make text white if bg is dark
-           option.style += 'color: #fff;';
-       }
-       $target.append($('<span>', option).text(hist[162+v]));
-    }
-    
-    $('body').append($target);
-}
+		// Quantization
+		for(i=0; i<2; i++)
+		{
+			if(maxindex[i]<162)
+			{
+				var H = Math.floor(maxindex[i]/9);
+				C[2*i] = Math.floor(H/45)*45+22.5;
+				C[2*i+1] = H<22.5?360:Math.ceil((H-22.5)/45)*45;
+			}
+			else
+			{
+				var S = maxindex[i]-162;
+				C[2*i] = C[2*i+1] = -S;
+			}
+		}
+		return C;
+	}
+	function showHist(hist){
+		var h, s, v, $target = $('<div>'), option;
+		for(h=0; h<18; ++h){
+				for(s=0; s<3; ++s){
+						for(v=0; v<3; ++v){
+								option = {style: 'background: hsl('+(h/18*360)+','+((s+1)/4*100)+'%,'+(v/3*100)+'%);'};
+								if(v === 0){ // make text white if bg is dark
+										option.style += 'color: #fff;';
+								}
+								$target.append($('<span>', option).text(hist[9*h+3*s+v]));
+						}
+				}
+				$target.append($('<br />'));
+		}
+		for(v=0; v<4; ++v){
+			 option = {style: 'background: hsl(0,0%,'+(v/4*100)+'%);'};
+			 if(v === 0){ // make text white if bg is dark
+					 option.style += 'color: #fff;';
+			 }
+			 $target.append($('<span>', option).text(hist[162+v]));
+		}
+
+		$('body').append($target);
+	}
+
+	// expose the functions
+	//
+	return {
+		distance: distance,
+		ColorF: ColorF,
+		RGB2HSV: RGB2HSV,
+		histHSV: histHSV,
+		showHist: showHist
+	}
+}());
